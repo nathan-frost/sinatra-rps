fa118ab7b0f40ea21024bffae9bc1144d4026b1d
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2024-04-10 21:21:30 +0000

    created layout and main methods

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+
+  moves = ["rock","paper","scissors"]
+
+  @cpu_move = moves.sample
+
+  if @cpu_move == "rock"
+    @outcome = "tied"
+  elsif @cpu_move == "paper"
+    @outcome = "lost"
+  elsif @cpu_move == "scissors"
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+
+  moves = ["rock","paper","scissors"]
+
+  @cpu_move = moves.sample
+
+  if @cpu_move == "rock"
+    @outcome = "won"
+  elsif @cpu_move == "paper"
+    @outcome = "tie"
+  elsif @cpu_move == "scissors"
+    @outcome = "lost"
+  end
+  erb(:rock)
+end
+
+get("/scissors") do
+
+  moves = ["rock","paper","scissors"]
+
+  @cpu_move = moves.sample
+
+  if @cpu_move == "rock"
+    @outcome = "lost"
+  elsif @cpu_move == "paper"
+    @outcome = "won"
+  elsif @cpu_move == "scissors"
+    @outcome = "tie"
+  end
+  erb(:rock)
 end
